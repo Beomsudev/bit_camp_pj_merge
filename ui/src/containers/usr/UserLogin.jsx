e68ec38cbc61aafae2e40daecd339da243291c8b
Beomsudev <bumsu@naver.com>
2020-11-11 16:43:40 +0900

    1111-8

--- ui/src/containers/usr/UserLogin.jsx
+++ ui/src/containers/usr/UserLogin.jsx
@@ -75,8 +75,9 @@ export default function UserLogin() {
 
             sessionStorage.setItem("sessionUser", res.data['usr_id'])
             
-            // window.location.reload()
             history.push("/")  // 여기 치는 부분으로 이동
+            window.location.reload()
+
         })
         .catch(error => {
             alert("Please check your ID or password.")
